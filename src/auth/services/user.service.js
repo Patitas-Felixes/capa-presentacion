30076e87f8883035ed929dce97657f041f96977d
AmirbarrabajaLeon <amircasan@gmail.com>
2025-07-06 23:06:43 -0500

    feat: implement UserService for user data management with API integration

--- src/auth/services/user.service.js
+++ src/auth/services/user.service.js
@@ -1,1 +1,11 @@
-//nothing for now...
+import { BaseService } from '../../shared/services/base-service.js';
+
+export class UserService extends BaseService {
+    constructor() {
+        // Al pasar '/usuarios', el metodo getAll heredado de BaseService
+        // sabrá que debe hacer la petición a `${VITE_API_BASE_URL}/usuarios`
+        super('/usuarios');
+    }
+}
+
+export const userService = new UserService();
